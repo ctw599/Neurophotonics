6bd70ea83980d91cf1e97377198f580664e163ff
Chemda Wiener <chemda10@gmail.com>
2024-06-05 11:59:10 +0300

    updated the code to work with main

--- CalcNIRS.m
+++ CalcNIRS.m
@@ -1,6 +1,4 @@
-
-function [dHbR, dHbO, fig] = CalcNIRS(dataFile, SDS, tissueType, extinctionCoefficientsFile, DPFperTissueFile, relDPFfile, plotChannelIdx)
-    % CalcNIRS - calculate and plot HbR HbO
+ % CalcNIRS - calculate and plot HbR HbO
     % Input:
     % dataFile - .mat file with intensity data.
     % DS.Lambda : two wavelengths (in nm)
@@ -18,100 +16,116 @@ function [dHbR, dHbO, fig] = CalcNIRS(dataFile, SDS, tissueType, extinctionCoeff
     % dHbO - HbO concentration change for all channels (nx20) where n is time vector length
     % fig - handle to figure. Empty if plotChannelIdx==[]
 
-    if nargin < 7
-        plotChannelIdx = [];
-    end
-
-    % Load the .mat data file
-    data = load(dataFile);
-    Lambda = data.SD.Lambda;
-    timeVector = data.t;
-    intensityData = data.d;
-    
-    % Read the extinction coefficients from the CSV file
-    extinctionData = readtable(extinctionCoefficientsFile);
-    extinctionData = renamevars(extinctionData,'x_SpecificExtinctionCoeffsTakenFromBORLWaterMeasuredByMatcherAt','Wavelength'); 
-    extinctionData = renamevars(extinctionData,'Var2','Water'); 
-    extinctionData = renamevars(extinctionData,'Var3','HbO2'); 
-    extinctionData = renamevars(extinctionData,'Var4','HHb'); 
-    extinctionData = renamevars(extinctionData,'Var5','FatSoybean'); 
-    extCoeffLambda1 = extinctionData{extinctionData.Wavelength == Lambda(1), {'HbO2', 'HHb'}};
-    extCoeffLambda2 = extinctionData{extinctionData.Wavelength == Lambda(2), {'HbO2', 'HHb'}};
-
-    % Read the DPF data for the given tissue type
-    DPFData = readtable(DPFperTissueFile, 'Delimiter', '\t');
-    DPF = DPFData{strcmp(DPFData.Tissue, tissueType), 'DPF'};
-
-    % Read the relative DPF coefficients
-    relDPFData = readtable(relDPFfile);
-    relDPF1 = relDPFData{relDPFData.wavelength == Lambda(1), 'relDPFcoeff'};
-    relDPF2 = relDPFData{relDPFData.wavelength == Lambda(2), 'relDPFcoeff'};
-
-    % Calculate the actual DPF for each wavelength
-    actualDPF1 = DPF * relDPF1;
-    actualDPF2 = DPF * relDPF2;
-
-    % Split intensity data into two wavelengths
-    I1 = intensityData(:, 1:20); % First 20 columns -> first wavelength
-    I2 = intensityData(:, 21:40); % Last 20 columns -> second wavelength
-
-    % Convert intensities to optical densities
-    OD1 = log(I1);
-    OD2 = log(I2);
-
-    % Calculate delta OD by subtracting the baseline (first measurement)
-    dOD1 = [-OD1 + OD1(1,:)];
-    dOD2 = [-OD2 + OD2(1,:)];
-
-    pathlength_1 = actualDPF1 * SDS;
-    pathlength_2 = actualDPF2 * SDS;
-
-    % Create the extinction coefficient matrix
-    % ASK IF TO DIVIDE OR TO MULTIPLY PATHLENGTH AND WHY TWO DIFFERENT WHEN
-    % THE SLIDE SHOWS ONLY ONE DIFFERENT
-    E = [extCoeffLambda1(1)/pathlength_1, extCoeffLambda1(2)/pathlength_1; ...
-         extCoeffLambda2(1)/pathlength_2, extCoeffLambda2(2)/pathlength_2];
-
-    % Inverse of the extinction coefficient matrix
-    invE = inv(E);
-
-    % Calculate changes in HbO and HbR concentrations for all channels
-    dHbO = zeros(size(dOD1));
-    dHbR = zeros(size(dOD1));
-
-    for i = 1:20
-        dOD = [dOD1(:,i), dOD2(:,i)];
-        dC = invE * dOD';
-        dHbO(:, i) = dC(1,:);
-        dHbR(:, i) = dC(2,:);
-    end
-
-    % Plot if required
-    if ~isempty(plotChannelIdx)
-        fig = figure;
-        hold on;
-        for i = plotChannelIdx
-            subplot(length(plotChannelIdx), 1, find(plotChannelIdx == i));
-            plot(timeVector, dHbO(:,i), 'r', 'DisplayName', 'HbO');
-            hold on;
-            plot(timeVector, dHbR(:,i), 'b', 'DisplayName', 'HbR');
-            title(['Channel ' num2str(i)]);
-            xlabel('Time (s)');
-            ylabel('Concentration Change');
-            legend show;
+function [dHbR, dHbO, fig] = CalcNIRS(dataFile, SDS, tissueType, extinctionCoefficientsFile, DPFperTissueFile, relDPFfile, plotChannelIdx)
+    if isfile(dataFile) && isfile(extinctionCoefficientsFile) && isfile(DPFperTissueFile) && isfile(relDPFfile) && isa(SDS, 'double') && isa(tissueType, 'char')
+        if nargin < 7
+            plotChannelIdx = [];
         end
-        hold off;
+    
+        % Load the main data file with the intensity
+        data = load(dataFile);
+        Lambda = data.SD.Lambda;
+        timeVector = data.t;
+        intensityData = data.d;
+        
+        % Read the extinction coefficients from the CSV file and extract
+        % the values for the Hb02 and the HHb for our further analysis.
+        % This is done for both of the wavelengths of interest (both of the
+        % lambdas.
+        extinctionData = readtable(extinctionCoefficientsFile);
+        extCoeffLambda1 = extinctionData{extinctionData.wavelength == Lambda(1), {'HbO2', 'HHb'}};
+        extCoeffLambda2 = extinctionData{extinctionData.wavelength == Lambda(2), {'HbO2', 'HHb'}};
+    
+        % Read the DPF data specific for the tissue type that is inputted
+        DPFData = readtable(DPFperTissueFile, 'Delimiter', '\t');
+        DPF = DPFData{strcmp(DPFData.Tissue, tissueType), 'DPF'};
+    
+        % Read the relative DPF coefficients - this is specific for each
+        % wavelength
+        relDPFData = readtable(relDPFfile);
+        relDPF1 = relDPFData{relDPFData.wavelength == Lambda(1), 'relDPFcoeff'};
+        relDPF2 = relDPFData{relDPFData.wavelength == Lambda(2), 'relDPFcoeff'};
+    
+        % We then multiply the wavelength DPF (relative DPF) by the DPF
+        % specific to the tissue, to get the DPF that we will use in our
+        % further calculations
+        totalDPF1 = DPF * relDPF1;
+        totalDPF2 = DPF * relDPF2;
+    
+        % Split intensity data into two wavelengths - the first 20 columns
+        % are the first wavelength and the last 20 columnds are the second
+        % wavelength
+        I1 = intensityData(:, 1:20);
+        I2 = intensityData(:, 21:40);
+    
+        % In order to calculate the change in optical density we take the log
+        % difference between the intensity at the beginning with the
+        % intensity at every time point - we do this for each wavelength
+        % again
+        OD1 = log(I1);
+        OD2 = log(I2);
+        dOD1 = [-OD1 + OD1(1,:)];
+        dOD2 = [-OD2 + OD2(1,:)];
+    
+        % The total pathlength is the total DPF multiplied by the length
+        % it has to travel which is the SDS - the source detector
+        % separation
+        pathlength_1 = totalDPF1 * SDS;
+        pathlength_2 = totalDPF2 * SDS;
+    
+        % We created a matrix that calculates for each wavelength and for
+        % each pathlength, the extinction coefficient so we could then
+        % multiply it by the intensity and get all of the values for each
+        % of the wavelengths
+        lambda1HbO = extCoeffLambda1(1);
+        lambda1HbR = extCoeffLambda1(2);
+        lambda2HbO = extCoeffLambda2(1);
+        lambda2HbR = extCoeffLambda2(2);
+        
+        E = [lambda1HbO/pathlength_1, lambda1HbR/pathlength_1; ...
+             lambda2HbO/pathlength_2, lambda2HbR/pathlength_2];
+    
+        % We take the inverse (as shown in the powerpoint)
+        invE = inv(E);
+    
+        % We initiate a matrix to calculate for all channels at every type
+        % point
+        dHbO = zeros(size(dOD1));
+        dHbR = zeros(size(dOD1));
+    
+        % For each of the wavelengths (represented by the dOD1 and dOD2
+        % calculations that we did) we calculate the coefficients multiplied by  the
+        % intensity to get the blood concentration at every point for every
+        % column 
+        for i = 1:20
+            % We take apart the matrix and assign the values to the
+            % relevant HbO part and HbR part
+            dOD = [dOD1(:,i), dOD2(:,i)];
+            dC = invE * dOD';
+            dHbO(:, i) = dC(1,:);
+            dHbR(:, i) = dC(2,:);
+        end
+    
+        % Plot if required - only if we give channel indices to plot
+        if ~isempty(plotChannelIdx)
+            fig = figure;
+            hold on;
+            for i = plotChannelIdx
+                subplot(length(plotChannelIdx), 1, find(plotChannelIdx == i));
+                plot(timeVector, dHbO(:,i), 'r', 'DisplayName', 'HbO');
+                hold on;
+                plot(timeVector, dHbR(:,i), 'b', 'DisplayName', 'HbR');
+                title(['Channel ' num2str(i)]);
+                xlabel('Time (s)');
+                ylabel('Concentration Change');
+                legend show;
+            end
+            hold off;
+        else
+            fig = [];
+        end
     else
-        fig = [];
+        disp("There are missing parameters or parameters of the wrong type")
     end
-    figure
-    Ts = 2823/361.2160;
-    y = fft(intensityData(:,1));
-    fs = 1/Ts;
-    f = (0:length(y)-1)*fs/length(y);
-    plot(f,abs(y))
-    xlabel('Frequency (Hz)')
-    ylabel('Magnitude')
-    title('Magnitude')
 end
 
